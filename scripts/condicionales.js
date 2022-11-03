a87d04a7bb72acab2fac84f8055b3a8c1511fd13
Andres Barrios <apaba06@gmail.com>
2022-11-03 10:54:26 -0300

    clase 8

---
+++ scripts/condicionales.js
@@ -0,0 +1,24 @@
+/*Tematica +15*/
+
+let edad = prompt("¿Cual es tu edad?")
+
+if (edad >= 15) {
+    alert("Puedes pasar.")
+}
+else{
+    alert("No podes pasar.")
+}
+
+let array_vacio = [];
+let mascota = "perro";
+
+if (mascota === "perro")
+
+
+/*Crea un array de strings con 5 elementos que quieras.
+Luego, mostrá en consola:
+1. El array
+2. Cuántos elementos tiene el array
+3. El índice 0 del array
+ */
+
